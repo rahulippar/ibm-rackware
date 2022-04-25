feeeeaefdeabd97b51ae91180af783f4bd164e8d
rahulippar <rahul.ippar@gmail.com>
2022-04-25 17:50:05 +0530

    Temporarily adding required_providers block
    Temporarily adding required_providers block

--- v2vrmm.tf
+++ v2vrmm.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    ibm = {
+      source  = "ibm-cloud/ibm"
+      #version = "1.38.0"
+    }
+  }
+  required_version = ">= 0.12"
+}
+
 provider "ibm" {
   ibmcloud_api_key = var.ibmcloud_api_key
   region = var.ibm_region
