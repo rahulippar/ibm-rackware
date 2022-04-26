981c939f74f54d32b61ca4fbfc282e2a3069454e
rahulippar <rahul.ippar@gmail.com>
2022-04-26 17:25:16 +0530

    Temp required provider changes
    Temp required provider changes

--- v2vrmm.tf
+++ v2vrmm.tf
@@ -1,12 +1,12 @@
 terraform {
-  required_providers {
-    ibm = {
-      source = "IBM-Cloud/ibm"
-      #version = "1.38.0"
+    required_providers {
+        ibm = {
+        source = "IBM-Cloud/ibm"
+        version = "1.40.1"
+        }
     }
-  }
-  #required_version = "0.15"
 }
+
 
 provider "ibm" {
   ibmcloud_api_key = var.ibmcloud_api_key
