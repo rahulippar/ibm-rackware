2f5100a01798c6b05f1548b2680828b190d7e31f
rahulippar <rahul.ippar@gmail.com>
2022-04-26 15:39:25 +0530

    Update v2vrmm.tf
    Adding required provider block

--- v2vrmm.tf
+++ v2vrmm.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+      #version = "1.38.0"
+    }
+  }
+  required_version = "0.15"
+}
+
 provider "ibm" {
   ibmcloud_api_key = var.ibmcloud_api_key
   region           = var.ibm_region
